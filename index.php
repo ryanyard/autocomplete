36c3e8217e087d956495de640120db80da90fa30
ryanyard <ryard@thecause.net>
2017-11-25 08:56:14 -0600

    added circle

--- index.php
+++ index.php
@@ -6,9 +6,10 @@
   <link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.1/themes/base/minified/jquery-ui.min.css" type="text/css" /> 
 </head>
 <body> 
+<img alt="Discerning Feline" src="https://storage.googleapis.com/autocomplete-central-images/hats4cats.jpg" title="hats4cats"/>
 
     <form action='' method='post'>
-        <p><label>Product:</label><input type='text' name='name' value='' class='auto'></p>
+        <p><label>For the Discerning Feline:</label><input type='text' name='name' value='' class='auto'></p>
     </form>
 
 <script type="text/javascript" src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
